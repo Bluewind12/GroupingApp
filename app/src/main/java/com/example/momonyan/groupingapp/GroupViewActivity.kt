397ec3af568f1381adc221054afb423c9e97a584
BlueWind <zero18030@gmail.com>
2018-09-13 00:15:23 +0900

    とりあえずのリスト表示

--- app/src/main/java/com/example/momonyan/groupingapp/GroupViewActivity.kt
+++ app/src/main/java/com/example/momonyan/groupingapp/GroupViewActivity.kt
@@ -5,18 +5,24 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import kotlinx.android.synthetic.main.group_view.*
 
 class GroupViewActivity : AppCompatActivity() {
-    private lateinit var listView: ListView
+//    private lateinit var groupView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContentView(R.layout.group_view)
         var memberString = intent.getStringExtra("String")
         val memberStrings = memberString.split("\n")
         val groupNum = intent.getIntExtra("Num", 1)
 
         Log.d("入力文字",getString(R.string.inputDebug,0,memberStrings[0]))
-//        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, memberStrings)
-//        listView = findViewById(R.id.groupListView)
-//        listView.adapter = arrayAdapter
+        val items = mutableListOf<String>()
+        for(i in 0 until memberStrings.size) {
+            items.add(i,memberStrings[i])
+        }
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
+//        groupView = findViewById(R.id.groupListView)
+//        groupView.adapter = arrayAdapter
     }
 }
