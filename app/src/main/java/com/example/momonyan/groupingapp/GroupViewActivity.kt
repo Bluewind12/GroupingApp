83b3c514998a22e229a68213516afdb7d6630b3e
BlueWind <zero18030@gmail.com>
2018-09-26 00:00:41 +0900

    ランダムでグループ分け（若干の不具合あり）

--- app/src/main/java/com/example/momonyan/groupingapp/GroupViewActivity.kt
+++ app/src/main/java/com/example/momonyan/groupingapp/GroupViewActivity.kt
@@ -12,12 +12,20 @@ class GroupViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var memberString = intent.getStringExtra("String")
-        val shuffleStrings = memberString.split("\n").shuffled()
+        val shuffleStrings = memberString.split("\n").shuffled().filterNotNull()
         val groupNum = intent.getIntExtra("Num", 1)
 
         val items = mutableListOf<String>()
+        var listCount = 0
+        var groupCount = 1
         for (i in 0 until shuffleStrings.size) {
-            items.add(i, shuffleStrings[i])
+            if(i %(shuffleStrings.size / groupNum) == 0){
+                items.add(listCount,"Group"+groupCount)
+                listCount++
+                groupCount++
+            }
+            items.add(listCount, shuffleStrings[i])
+            listCount++
         }
         val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
 //        groupView = findViewById(R.id.groupListView)
